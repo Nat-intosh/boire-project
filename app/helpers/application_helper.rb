93e7cd2ddc6efa75da92fc7b7a78bf3699201be1
Nat-intosh <nvandaele1234@gmail.com>
2024-05-21 12:33:09 +0200

    les emojis trop mimisssss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def emoji_for_card_type(card_type)
+        case card_type
+        when 'hot'
+          '🌶️'
+        when 'drink'
+          '🍻'
+        when 'defi'
+          '🎲'
+        when 'gametime'
+          '▶'
+        else
+          '🍻' # Default emoji for unknown card types
+        end
+      end
 end
